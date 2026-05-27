f638cd0372374fbb379236e3e10f071abc75ffaf
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-05-26 23:24:33 -0300

    style: estilizando a memoria

--- src/components/memory/index.jsx
+++ src/components/memory/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import memoria from "../../../processador/Memory";
+import styles from './styles.module.css'
 
 export default function Memory() {
     const [, forceUpdate] = useState(0);
@@ -13,14 +14,27 @@ export default function Memory() {
     }, []);
 
     return (
-        <>
-            {
-                memoria.data.map((value, index) => (
-                    <div key={index}>
-                        {index}: {value}
-                    </div>
-                ))
-            }
-        </>
+        <div className={styles.memory}>
+            <div className={styles.title}>
+                Memória
+            </div>
+            <div className={styles.container}>
+                {
+                    memoria.data.map((value, index) => (
+                        <div key={index}>
+                            <div className={styles.adress}>
+                                <div className={styles.index}>
+                                    {index} :
+                                </div>
+                                <div className={styles.value}>
+                                    {value}
+                                </div>
+                            </div>
+                            <div className={styles.division}></div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     );
 }
