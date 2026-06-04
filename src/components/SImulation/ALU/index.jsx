6795d34f92252b3c47d196f6cb5bcbcd996afa60
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-06-04 03:37:24 -0300

    style: estilização inicial do simulador

---
+++ src/components/SImulation/ALU/index.jsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css'
+
+export default function ALU({ label, value, className }) {
+    return (
+        <div className={`${styles.aluContainer} ${className || ""}`}>
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 120 70"
+                    preserveAspectRatio="xMidYMid meet"
+                >
+                <polygon
+                    points="10,10 42,10 50,30 70,30 78,10 110,10 95,60 25,60"
+                    fill="transparent"
+                    stroke="var(--gray-900)"
+                    strokeWidth="0.5"
+                />
+            </svg>
+
+            <div className={styles.content}>
+                <header className={styles.label}>
+                    {label}
+                </header>
+
+                <div className={styles.value}>
+                    <h1>{value}</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/SImulation/index.jsx
+++ src/components/SImulation/index.jsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.css'
 import { useEffect, useState } from 'react'
 import {uc} from '../../../mac1/teste3' // instância exportada
+import ALU from './ALU';
 
 const estadoInicial = {
     subciclo: 1, ciclos: 0,
@@ -21,16 +22,16 @@ const estadoInicial = {
     
 };
 
-function COMPONENTE({ label, value, customStyle }) {
+function COMPONENTE({ label, value, className }) {
     return (
-        // Passe a prop para o style da div principal
-        <div className={styles.componente} style={customStyle}>
+        <div
+            className={`${styles.componente} ${className || ""}`}>
             <header className={styles.label}>
                 {label}
             </header>
-            <span className={styles.value}>
-                {value}
-            </span>
+            <div className={styles.value}>
+                <h1>{value}</h1>
+            </div>
         </div>
     );
 }
@@ -56,7 +57,7 @@ function BancoDeRegistradores({ estado , customStyle}) {
         { id: 'regF', label: 'REG F' }
     ];
     return (
-        <div className={styles.banco} style={customStyle}>
+        <div className={styles.banco}>
             <div className={styles.tituloBanco} >Banco de Registradores</div>
             
             <div className={styles.registradoresGrid}>
@@ -82,15 +83,42 @@ export default function Simulation() {
         uc.setCallback((novoEstado) => {
             setEstado(novoEstado);
         });
-
-        return () => {
-            uc.setCallback(null);
-        };
     }, []);
 
     return (
         <div className={styles.simulation}>
-            {/* 
+
+            <BancoDeRegistradores estado={estado}/>
+            
+          
+            <COMPONENTE label="Clock" value={`Ciclos : ${estado.ciclos} |  Subciclo : ${estado.subciclo}`} className={styles.clock}/>
+            <COMPONENTE label="DEC A" value={estado.decA} className={styles.decA} />
+            <COMPONENTE label="DEC B" value={estado.decB} className={styles.decB} />
+            <COMPONENTE label="DEC C" value={estado.decC} className={styles.decC} />
+
+            <COMPONENTE label="MIR" value={estado.mir} className={styles.mir}/>
+            
+            
+            <COMPONENTE label="MPC" value={estado.mpc} className={styles.mpc}/>
+            <COMPONENTE label="MSL" value={estado.msl} className={styles.msl}/>
+            <COMPONENTE label="MMUX" value={estado.mmux} className={styles.mmux}/>
+            
+            <COMPONENTE label = "Control Store" value="256 x 32" className={styles.controlStore}/>
+            
+            <COMPONENTE label="MAR" value={estado.mar} className={styles.mar}/>
+            <COMPONENTE label="MBR" value={estado.mbr} className={styles.mbr}/>
+            <COMPONENTE label="Latch A" value={estado.latA} className={styles.latchA}/>
+            <COMPONENTE label="Latch B" value={estado.latB} className={styles.latchB}/>
+            <COMPONENTE label="AMUX" value={estado.amux} className={styles.amux}/>
+            <ALU label="ULA" value={`${estado.aluRes} (Z=${estado.aluZ} N=${estado.aluN})`} className={styles.alu}/>
+            <COMPONENTE label="Shifter" value={estado.shifter} className={styles.shifter}/>
+
+            
+        </div>
+    );
+}
+
+{/* 
             <header>
                 <h1>Ciclos: {estado.ciclos} — Subciclo: {estado.subciclo}</h1>
             </header>
@@ -122,33 +150,3 @@ export default function Simulation() {
                 <p>Shifter: {estado.shifter}</p>
             </section>
              */}
-
-            <BancoDeRegistradores estado={estado} customStyle={{position: 'absolute', top: '5%', left: '0%'}}/>
-            
-          
-            <COMPONENTE label="Clock" value={`Ciclos : ${estado.ciclos} |  Subciclo : ${estado.subciclo}`} customStyle={{ position: 'absolute', top: '5%', left: '80%' }} />
-            <COMPONENTE label="DEC A" value={estado.decA} customStyle={{ position: 'absolute', top: '5%', left: '60%' }} />
-            <COMPONENTE label="DEC B" value={estado.decB} customStyle={{ position: 'absolute', top: '15%', left: '60%' }} />
-            <COMPONENTE label="DEC C" value={estado.decC} customStyle={{ position: 'absolute', top: '25%', left: '60%' }} />
-
-            <COMPONENTE label="MIR" value={estado.mir} customStyle={{ position: 'absolute', top: '65%', left: '85%' }} />
-            
-            
-            <COMPONENTE label="MPC" value={estado.mpc} customStyle={{ position: 'absolute', top: '30%', left: '85%'  }} />
-            <COMPONENTE label="MSL" value={estado.msl} customStyle={{ position: 'absolute', top: '75%', left: '55%' }} />
-            <COMPONENTE label="MMUX" value={estado.mmux} customStyle={{ position: 'absolute', top: '20%', left: '83%'  }} />
-            
-            <COMPONENTE label = "Control Store" value="256 x 32" customStyle={{ position: 'absolute', top: '40%', left: '85%' }}/>
-            
-            <COMPONENTE label="MAR" value={estado.mar} customStyle={{ position: 'absolute', top: '60%', left: '0%' }} />
-            <COMPONENTE label="MBR" value={estado.mbr} customStyle={{  position: 'absolute', top: '70%', left: '0%'  }} />
-            <COMPONENTE label="Latch A" value={estado.latA} customStyle={{position: 'absolute', top: '50%', left: '20%'    }} />
-            <COMPONENTE label="Latch B" value={estado.latB} customStyle={{ position: 'absolute', top: '50%', left: '40%'    }} />
-            <COMPONENTE label="AMUX" value={estado.amux} customStyle={{ position: 'absolute', top: '65%', left: '20%'   }} />
-            <COMPONENTE label="ALU" value={`${estado.aluRes} (Z=${estado.aluZ} N=${estado.aluN})`} customStyle={{position: 'absolute', top: '75%', left: '27%'   }} />
-            <COMPONENTE label="Shifter" value={estado.shifter} customStyle={{ position: 'absolute', top: '85%', left: '30%'  }} />
-
-            
-        </div>
-    );
-}
